071fbb51cd8bfc2f356434bf94ac9f98a023238c
Sander Tonheim <sandert.kodehode@gmail.com>
2022-09-26 13:23:24 +0200

    Navbar added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,14 @@
+import { NavbarStyled } from '../styled/NavBar.styled'
+import { ReactComponent as Logo } from '../assets/Logo.svg'
 export const NavBar = () => {
-  return 
+	return (
+		<NavbarStyled className='NavBar'>
+			<ul>
+				<li>Foks AS</li>
+				<li>
+					<Logo />
+				</li>
+			</ul>
+		</NavbarStyled>
+	)
 }
